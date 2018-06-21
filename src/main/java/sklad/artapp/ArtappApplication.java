09e7eb14dd22dc98fc0d39c1805d6d7224de4372
Agnieszka Chudzicka <agnieszkadanuta.chudzicka@gmail.com>
2018-06-21 18:53:03 +0200

    Initial commit

---
+++ src/main/java/sklad/artapp/ArtappApplication.java
@@ -0,0 +1,12 @@
+package sklad.artapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArtappApplication.class, args);
+    }
+}
---
+++ src/test/java/sklad/artapp/ArtappApplicationTests.java
@@ -0,0 +1,16 @@
+package sklad.artapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ArtappApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
